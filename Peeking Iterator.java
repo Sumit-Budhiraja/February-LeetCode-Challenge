f40dbf651ba1d2817b3b112f86862f9307cea172
Sumit Budhiraja <48406445+Sumit-Budhiraja@users.noreply.github.com>
2021-02-09 00:24:22 +0530

    Add files via upload

---
+++ Peeking Iterator.java
@@ -0,0 +1,33 @@
+class PeekingIterator implements Iterator<Integer> {
+    // Peek element
+    Integer nextEl = null;
+    Iterator<Integer> it = null;
+    public PeekingIterator(Iterator<Integer> iterator) {
+        // initialize any member here.
+        this.it = iterator;
+        nextEl = it.next();
+    }
+
+    // Returns the next element in the iteration without advancing the iterator.
+    public Integer peek() {
+        return nextEl;
+    }
+
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    @Override
+    public Integer next() {
+        Integer valueToReturn = nextEl;
+        if(it.hasNext()){
+            this.nextEl = it.next();
+        } else{
+            this.nextEl = null;
+        }
+        return valueToReturn;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return nextEl!=null;
+    }
+}
